260d8fc63a467d9a8b57879d8e7d980c485d1676
uwx <maxine@cybergal.com>
2025-01-05 17:16:40 -0300

    Do template

--- index.js
+++ index.js
@@ -1,48 +1,51 @@
 #!/usr/bin/env node
 
-const spawn = require('cross-spawn');
-const fs = require('fs');
-const path = require('path');
+// @ts-check
+
+import { sync as spawnSync } from 'cross-spawn';
+import { existsSync } from 'node:fs';
+import { mkdir, cp, rename, writeFile } from 'node:fs/promises';
+import { resolve, join } from 'node:path';
+import { pathToFileURL } from 'node:url';
 
 // The first argument will be the project name.
 const projectName = process.argv[2];
 
+if (!projectName) {
+    throw new Error('Please pass a project name as an argument');
+}
+
 // Create a project directory with the project name.
 const currentDir = process.cwd();
-const projectDir = path.resolve(currentDir, projectName);
-fs.mkdirSync(projectDir, { recursive: true });
+const projectDir = resolve(currentDir, projectName);
+
+if (existsSync(projectDir)) {
+    throw new Error('Folder already exists!');
+}
+
+await mkdir(projectDir, { recursive: true });
 
 // A common approach to building a starter template is to
 // create a `template` folder which will house the template
 // and the files we want to create.
-const templateDir = path.resolve(__dirname, 'template');
-fs.cpSync(templateDir, projectDir, { recursive: true });
+const templateDir = resolve(import.meta.dirname, 'template');
+await cp(templateDir, projectDir, { recursive: true });
 
-// It is good practice to have dotfiles stored in the
-// template without the dot (so they do not get picked
-// up by the starter template repository). We can rename
-// the dotfiles after we have copied them over to the
-// new project directory.
-fs.renameSync(
-  path.join(projectDir, 'gitignore'),
-  path.join(projectDir, '.gitignore')
+const { default: projectPackageJson } = await import(
+    pathToFileURL(join(projectDir, 'package.json')).href,
+    { with: { type: 'json' } }
 );
-
-const projectPackageJson = require(path.join(projectDir, 'package.json'));
 
 // Update the project's package.json with the new project name
 projectPackageJson.name = projectName;
 
-fs.writeFileSync(
-  path.join(projectDir, 'package.json'),
-  JSON.stringify(projectPackageJson, null, 2)
-);
+await writeFile(join(projectDir, 'package.json'), JSON.stringify(projectPackageJson, null, 4));
 
-// Run `npm install` in the project directory to install
+// Run `pnpm install` in the project directory to install
 // the dependencies. We are using a third-party library
 // called `cross-spawn` for cross-platform support.
 // (Node has issues spawning child processes in Windows).
-spawn.sync('npm', ['install'], { stdio: 'inherit' });
+spawnSync('pnpm', ['install'], { stdio: 'inherit', cwd: projectDir });
 
 console.log('Success! Your new project is ready.');
 console.log(`Created ${projectName} at ${projectDir}`);
